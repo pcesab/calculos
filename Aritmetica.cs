54158cf6fadbd05b064c7c8cbdcb8ce3d246c978
Pedro Costa <96047553+pcesab@users.noreply.github.com>
2025-01-24 20:40:05 +0000

    Criar classe Aritmetica e adicionar método Somar()

---
+++ Aritmetica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    internal class Aritmetica
+    {
+        /// <summary>
+        /// Implementa operações aritméticas.
+        /// </summary>
+
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
